9d89f692a5d821a932a8505f2a5eb7c04dcb7467
user <user@COM11>
2018-05-23 20:58:03 +0900

    ishs chapter03

--- src/icehs/science/chapter03/TestScore.java
+++ src/icehs/science/chapter03/TestScore.java
@@ -3,7 +3,16 @@ package icehs.science.chapter03;
 public class TestScore {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int javaScore = 100;
+		int sqlScore = 80;
+		int servletJSPScore = 85;
+		
+		System.out.println(javaScore);
+		System.out.println(sqlScore);
+		System.out.println(servletJSPScore);
+		System.out.println(servletJSPScore);
+		System.out.println(servletJSPScore);
+		
 
 	}
 
